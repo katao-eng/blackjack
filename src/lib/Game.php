717d7f0b7346bf68384a45ab2273a345988d52d2
katao-eng <o_k.2o11@icloud.com>
2023-03-24 19:49:52 +0900

    fix: ゲームの流れ

--- src/lib/Game.php
+++ src/lib/Game.php
@@ -5,46 +5,35 @@ namespace Blackjack;
 require_once('Deck.php');
 require_once('Player.php');
 require_once('Dealer.php');
+require_once('Hand.php');
 
 class Game
 {
     private const FACE_DOWN_CARD_FLG = false;
 
     private $deck;
-    private $player1;
+    private $player;
     private $dealer;
 
     public function start(): void
     {
         $this->deck = new Deck();
-        $this->player1 = new Player('あなた');
+        $this->player = new Player();
         $this->dealer = new Dealer();
 
         echo 'ブラックジャックを開始します。' . PHP_EOL;
-        $this->player1->addCard($this->deck->dealCard());
-        $this->player1->addCard($this->deck->dealCard());
+        $this->player->addCard($this->deck->dealCard());
+        $this->player->addCard($this->deck->dealCard());
         $this->dealer->addCard($this->deck->dealCard());
         $this->dealer->addCard($this->deck->dealCard(), self::FACE_DOWN_CARD_FLG);
         // プレイヤーのターン
-        $this->player1->hitOrStand();
+        $this->player->hitOrStand();
         // ディーラーのターン
         $this->dealer->showSecondCard();
         $this->dealer->hitOrStand();
 
-        $this->player1->showHandValue();
+        $this->player->showHandValue();
         $this->dealer->showHandValue();
-        $this->compareHands();
-        echo 'ブラックジャックを終了します。';
-    }
-
-    private function compareHands(): void
-    {
-        if ($this->player1->getValue() > $this->dealer->getValue) {
-            echo "{$this->player1->name}の勝ちです!";
-        } elseif ($this->player1->getValue() < $this->dealer->getValue) {
-            echo "{$this->dealer->name}の勝ちです!";
-        } else {
-            echo '引き分けです。';
-        }
+        Hand::compareHands($this->player, $this->dealer);
     }
 }

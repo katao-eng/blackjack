a07534a6d7cbe7022366abce7f5c512716bdf6c5
katao-eng <o_k.2o11@icloud.com>
2023-03-24 19:45:55 +0900

    create: Deckクラス

---
+++ src/lib/Deck.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Blackjack;
+
+require_once('Card.php');
+
+class Deck
+{
+    private const SUITS = array('ハート', 'クラブ', 'スペード', 'ダイヤ');
+    private const VALUES = array('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K');
+
+    /**
+     * Summary of cards
+     * @var array<int, Card>
+     */
+    private array $cards = array();
+
+    public function __construct()
+    {
+        foreach (self::SUITS as $suit) {
+            foreach (self::VALUES as $value) {
+                $card = new Card($suit, $value);
+                array_push($this->cards, $card);
+            }
+        }
+
+        shuffle($this->cards);
+    }
+
+    public function dealCard(): Card
+    {
+        return array_pop($this->cards);
+    }
+
+    /**
+     * @return array<int, Card>
+     */
+    public function getCards(): array
+    {
+        return $this->cards;
+    }
+}

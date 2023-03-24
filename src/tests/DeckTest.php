defe132f46edae164592547c5d9fee0b655f106e
katao-eng <o_k.2o11@icloud.com>
2023-03-24 19:30:43 +0900

    add: DeckTest テストケース

--- src/tests/DeckTest.php
+++ src/tests/DeckTest.php
@@ -9,13 +9,6 @@ require_once(__DIR__ . '../../lib/Deck.php');
 
 class DeckTest extends TestCase
 {
-    public function testDeckSize(): void
-    {
-        $deck = new Deck();
-        $cards = $deck->getCards();
-        $this->assertCount(52, $cards);
-    }
-
     public function testDealCard(): void
     {
         $deck = new Deck();
@@ -32,5 +25,11 @@ class DeckTest extends TestCase
 
         $this->assertContains($card1->getSuit, $suits);
         $this->assertContains($card1->getValue, $values);
+    }
+
+    public function testGetCards(): void
+    {
+        $deck = new Deck();
+        $this->assertCount(52, $deck->getCards());
     }
 }

f0c000c295cac83f24e4bbd096616cf3a36c9635
katao-eng <o_k.2o11@icloud.com>
2023-03-28 20:38:20 +0900

    create: Playerクラス

---
+++ src/lib/Player.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Blackjack;
+
+use Blackjack\Hand;
+use Blackjack\Deck;
+
+class Player extends Hand
+{
+    public function __construct()
+    {
+        $this->name = 'あなた';
+    }
+
+    public function hitOrStand(Deck $deck): void
+    {
+        do {
+            echo $this->getCurrentValueMsg() . PHP_EOL . self::CARD_DRAW_MSG . PHP_EOL;
+            $input = trim(fgets(STDIN));
+
+            while (!in_array($input, self::INPUT_CHECK)) {
+                echo self::YN_INPUT_MSG . PHP_EOL;
+                echo $this->getCurrentValueMsg() . PHP_EOL . self::CARD_DRAW_MSG . PHP_EOL;
+                $input = trim(fgets(STDIN));
+            }
+
+            if ($input === 'Y') {
+                $drawCard = $deck->dealCard();
+                $this->addCard($drawCard);
+                $this->showDrawCard($drawCard);
+            }
+        } while ($input === 'Y');
+    }
+}
